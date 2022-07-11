997396ec1fe10c6f10a9a1d8a7efd23a435baad1
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2022-07-11 17:27:21 +0200

    Added functional testing with TDD

---
+++ DotNetSamples/C#/Unit Testing/CalculatorService/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorService
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (!input.Contains(','))
+            {
+                throw new ArgumentException("Does not contain ','. wrong input format");
+            }
+
+            string[] splittedInput = input.Split(',');
+
+            if (splittedInput.Count() > 2)
+            {
+                throw new ArgumentException("Does not meet format requirements");
+            }
+
+            int value1 = default!;
+            int value2 = default!;
+
+            if (!int.TryParse(splittedInput[0], out value1))
+            {
+                throw new FormatException("part one cannot be converted");
+            }
+
+            if (!int.TryParse(splittedInput[1], out value2))
+            {
+                throw new FormatException("part one cannot be converted");
+            }
+
+            return value1 + value2;
+        }
+    }
+}
---
+++ DotNetSamples/C#/Unit Testing/CalculatorServiceTests/CalculatorTests.cs
@@ -0,0 +1,51 @@
+using CalculatorService;
+namespace CalculatorServiceTests
+{
+    public class Tests
+    {
+        private readonly Calculator _calculatorService = new();
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void ShoudlReturnExceptionWhenInputNULL()
+        {
+            Assert.Throws<ArgumentNullException>(() => _calculatorService.Add(null));
+        }
+
+        [Test]
+        public void ShouldReturnExceptionWhenWrongFormat()
+        {
+            Assert.Throws<ArgumentException>(() => _calculatorService.Add("AB"));
+        }
+
+        [Test]
+        public void ShouldReturnExceptionWhenTooManyCommas()
+        {
+            Assert.Throws<ArgumentException>(() => _calculatorService.Add("A,B,C"));
+        }
+
+        [Test]
+        public void ShoudlReturnExceptionWhenPartOneCannotConvert()
+        {
+            Assert.Throws<FormatException>(() => _calculatorService.Add("A, 1"));
+        }
+
+        [Test]
+        public void ShoudlReturnExceptionWhenPartTwoCannotConvert()
+        {
+            Assert.Throws<FormatException>(() => _calculatorService.Add("1, A"));
+        }
+
+        [Test]
+        public void ShouldReturnResult()
+        {
+            Assert.AreEqual(expected: 3, actual: _calculatorService.Add("1,2"));
+        }
+
+    }
+}
