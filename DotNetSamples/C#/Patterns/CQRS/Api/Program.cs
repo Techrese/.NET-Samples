2416f6244b7a8241a522df424f13b632cc4b0bfd
TechnologyResearcher <tim.clinckemalie@hotmail.com>
2022-07-11 16:10:39 +0200

    Added inmemory database Added dI

--- DotNetSamples/C#/Patterns/CQRS/Api/Program.cs
+++ DotNetSamples/C#/Patterns/CQRS/Api/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Commands;
+using Commands.AddNewPart;
+using Commands.DeletePart;
+using Queries;
+using Queries.GetPartById;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICommandHandler<AddNewPartCommand>, AddNewPartCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<DeletePartCommand>, DeletePartCommandHandler>();
+builder.Services.AddScoped<IQueryHandler<GetPartByIdQuery>, GetPartByIdQueryHandler>();
+builder.Services.AddScoped<ICommandDispatcher, CommandDispatcher>();
+builder.Services.AddScoped<IQueryDispatcher, QueryDispatcher>();
+
+builder.Services.AddEntityFrameworkInMemoryDatabase().AddDbContext<ApplicationContextInMemory>(opt => opt.UseInMemoryDatabase(databaseName: "CQRS"), ServiceLifetime.Singleton);
+
 
 var app = builder.Build();
 
